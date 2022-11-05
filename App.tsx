37af99505735b88fd5bfb836c3af57381fccfd6a
leandrohorizon <leandro.mcontato@gmail.com>
2022-11-05 20:39:26 -0300

    Instala fontes, cria componente loading e cria tela de signin

--- App.tsx
+++ App.tsx
@@ -1,17 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
 import { NativeBaseProvider, Center, Text } from 'native-base';
 
+import { useFonts,
+         Roboto_400Regular,
+         Roboto_500Medium,
+         Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Loading } from './src/components/Loading';
+
 import { THEME } from './src/styles/theme';
+import { SignIn } from './src/screens/Signin';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
+
   return (
     <NativeBaseProvider theme={THEME}>
       <Center flex={1} bgColor="gray.900">
-        <Text color="white" fontSize={24}>
-          hello friend
-        </Text>
-        <StatusBar style="auto" />
+        {fontsLoaded ? <SignIn /> : <Loading />}
       </Center>
     </NativeBaseProvider>
   );
