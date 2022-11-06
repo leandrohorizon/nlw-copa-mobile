be87ad133567db7de6fab84fad1f86e8db79c838
leandrohorizon <leandro.mcontato@gmail.com>
2022-11-06 00:50:43 -0300

    Implementando imagem svg

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,11 +1,11 @@
 import { Center, Text } from 'native-base';
 
+import Logo from '../assets/logo.svg';
+
 export function SignIn() {
   return (
     <Center flex={1} bg="gray.900">
-      <Text color="white" fontSize={24} fontFamily="heading">
-        hello friend
-      </Text>
+      <Logo width={212} height={40} />
     </Center>
   );
 }
