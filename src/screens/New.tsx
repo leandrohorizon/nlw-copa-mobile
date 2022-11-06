5150348306a8f2bf84f8605431806df6f9a3be70
leandrohorizon <leandro.mcontato@gmail.com>
2022-11-06 17:14:14 -0300

    Listando bolões

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -1,12 +1,50 @@
-import { Heading, Text, VStack } from "native-base";
+import { Heading, Text, useToast, VStack } from "native-base";
+import { useState } from "react";
 
 import Logo from '../assets/logo.svg';
 
 import { Header } from "../components/Header";
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
+import { api } from "../services/api";
 
 export function New(){
+  const [title, setTitle] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toast = useToast();
+
+  async function handlePoolCreate(){
+    if(!title){
+      return toast.show({
+        title: 'Informe um nome para o seu bolão',
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    }
+    try {
+      setIsLoading(true);
+
+      await api.post('/pools', { title })
+
+      toast.show({
+        title: 'Bolão criado com sucesso',
+        placement: 'top',
+        bgColor: 'green.500',
+      });
+
+      setTitle('')
+
+    } catch (error) {
+      toast.show({
+        title: 'Não foi possível criar o seu bolão',
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
   return (
     <VStack flex={1} bgColor="gray.900">
       <Header title="Criar novo bolão" />
@@ -28,10 +66,14 @@ export function New(){
         <Input
           mb={2}
           placeholder="Qual o nome do seu bolão?"
+          onChangeText={setTitle}
+          value={title}
         />
 
         <Button
           title="CRIAR MEU BOLÃO"
+          onPress={handlePoolCreate}
+          isLoading={isLoading}
         />
 
         <Text
--- src/screens/Pools.tsx
+++ src/screens/Pools.tsx
@@ -1,12 +1,45 @@
-import { VStack, Icon } from "native-base";
+import { VStack, Icon, useToast, FlatList } from "native-base";
 import { Octicons } from '@expo/vector-icons';
 import { useNavigation } from "@react-navigation/native";
+import { useFocusEffect } from "@react-navigation/native";
 
 import { Button } from "../components/Button";
 import { Header } from "../components/Header";
 
+import { api } from "../services/api";
+import { useCallback, useEffect, useState } from "react";
+import { PoolCard, PoolCardsProps } from "../components/PoolCard";
+import { EmptyPoolList } from "../components/EmptyPoolList";
+import { Loading } from "../components/Loading";
+
 export function Pools(){
+  const [isLoading, setIsLoading] = useState(true);
+  const [pools, setPools] = useState<PoolCardsProps[]>([]);
+
   const { navigate } = useNavigation();
+  const toast = useToast();
+
+  async function fetch_pools(){
+    try {
+      setIsLoading(true);
+      const response = await api.get('/pools');
+
+      setPools(response.data.pools);
+
+    } catch (error) {
+      toast.show({
+        title: 'Não foi possível carregar os bolões',
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useFocusEffect(useCallback(() => {
+    fetch_pools();
+  }, []))
 
   return (
     <VStack flex={1} bgColor="gray.900">
@@ -25,6 +58,18 @@ export function Pools(){
           onPress={() => navigate('find')}
         />
       </VStack>
+    {
+      isLoading ? <Loading /> :
+        <FlatList
+          data={pools}
+          keyExtractor={item => item.id}
+          renderItem={({ item })=> <PoolCard data={item} />}
+          px={5}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ pb: 10 }}
+          ListEmptyComponent={() => <EmptyPoolList />}
+        />
+    }
     </VStack>
   )
 }
