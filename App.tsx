d1c4e9e7ee0637ad282edb58a0386e51f6ea4278
leandrohorizon <leandro.mcontato@gmail.com>
2022-11-06 15:33:12 -0300

    Implementa navegação na aplicação

--- App.tsx
+++ App.tsx
@@ -10,7 +10,7 @@ import { AuthContextProvider } from './src/context/AuthContext';
 import { THEME } from './src/styles/theme';
 import { Loading } from './src/components/Loading';
 
-import { SignIn } from './src/screens/SignIn';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
@@ -23,7 +23,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        {fontsLoaded ? <SignIn /> : <Loading />}
+        {fontsLoaded ? <Routes /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
